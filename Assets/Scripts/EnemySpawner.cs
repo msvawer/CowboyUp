2260409e18e70342fa2b926c89076bd1bceaf1ed
MichVawer <michelle.vawer@icloud.com>
2022-03-20 20:39:08 -0400

    object pooling for enemies done-ish

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -24,24 +24,36 @@ public class EnemySpawner : MonoBehaviour
         zRange = Random.Range(-3, 10);
         randomPosEnemy = new Vector3(-14, yRange, zRange);
         spawnRateSeconds = Random.Range(1, 5);
+
+        GameObject enemyPrefab = ObjectPool.SharedInstance.GetPooledObject();
+        if (enemyPrefab != null)
+        {
+            enemyPrefab.transform.position = randomPosEnemy;
+            enemyPrefab.transform.rotation = Quaternion.identity;
+            enemyPrefab.SetActive(true);
+        }
+
         StartCoroutine(SpawnEnemies());
     }
 
+
+
     IEnumerator SpawnEnemies()
     {
-        while (true)
+     while (true)
+    {
+    GameObject enemyPrefab = ObjectPool.SharedInstance.GetPooledObject();
+        if (enemyPrefab != null)
         {
-            Instantiate(enemyPrefab, randomPosEnemy, Quaternion.identity);
-
-            yield return new WaitForSeconds(spawnRateSeconds);
+            enemyPrefab.transform.position = randomPosEnemy;
+            enemyPrefab.transform.rotation = Quaternion.identity;
+            enemyPrefab.SetActive(true);
         }
+           //Instantiate(enemyPrefab, randomPosEnemy, Quaternion.identity);
 
-
-        
-
-
-
-    }
+          yield return new WaitForSeconds(spawnRateSeconds);
+        }
+  }
 
     
 }
