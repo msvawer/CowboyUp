4132c1718cbb0b435a2da6a23c4b24b8ab73b5e0
MichVawer <michelle.vawer@icloud.com>
2022-03-23 15:03:04 -0400

    playing with raycastigns scripts updated to urp

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -6,12 +6,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Gun : MonoBehaviour
 {
     private XRGrabInteractable interactable = null;
+    public GameObject laserprefab;
 
     public float damage = 10f;
-    public float range = 100f;
+    public float hitRange = 100f;
 
-    public void Fire()
+    public void Shoot()
     {
-        print("Fire");
+        RaycastHit hit;
+        if (Physics.Raycast(laserprefab.transform.position, laserprefab.transform.forward, out hit, hitRange))
+        {
+            Debug.Log(hit.transform.name);
+        }
     }
 }
