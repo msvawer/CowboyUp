27238fecf9ea29f98024703a00c6f92ae6fb6bf3
MichVawer <michelle.vawer@icloud.com>
2022-03-23 20:05:06 -0400

    adjustment to xr rig

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -3,20 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
 public class Gun : MonoBehaviour
 {
-   
-    public GameObject laserprefab;
+    private XRGrabInteractable interactable = null;
 
-    public float damage = 10f;
-    public float hitRange = 100f;
 
-    public void Shoot()
+    private void Awake()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(laserprefab.transform.position, laserprefab.transform.forward, out hit, hitRange))
-        {
-            Debug.Log(hit.transform.name);
-        }
+        interactable = GetComponent<XRGrabInteractable>();
     }
+
+    private void OnEnable()
+    {
+        interactable.onActivate.AddListener(Fire);
+
+    }
+
+    private void OnDisable()
+    {
+        interactable.onDeactivate.RemoveListener(Fire);
+    }
+
+    private void Fire(XRBaseInteractor interactor)
+    {
+        print("Fire");
+    }
+
+    //public GameObject laserprefab;
+
+   // public float damage = 10f;
+   // public float hitRange = 100f;
+
+    //public void Shoot()
+   // {
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(laserprefab.transform.position, laserprefab.transform.forward, out hit, hitRange))
+     //   {
+     //       Debug.Log(hit.transform.name);
+     //   }
+   // }
 }
